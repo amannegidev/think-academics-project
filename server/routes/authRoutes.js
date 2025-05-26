257922f74fd9aaef1e4db1d6ba65d967df6c82d0
amannegidev <amannegi2314@gmail.com>
2025-05-26 18:44:39 +0530

    fix: remove unused protected route handlers for cleaner code

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -25,16 +25,7 @@ router.post('/forgotpass', forgotPasswordController);
 
 
 
-//protected User route auth
-router.get('/user-auth',requireSignIn,(req,res) =>{
-    res.status(200).send({ok:true});
-});
 
-
-//pretected adimn route auth
-router.get('/admin-auth',requireSignIn, isAdmin,(req,res) =>{
-    res.status(200).send({ok:true});
-});
 
 
 export default router;

1a898de79d9d8220c4e1fd42cff8be5e264fa243
amannegidev <amannegi2314@gmail.com>
2025-05-26 20:02:45 +0530

    fix: refine styling in Categories component for improved layout and consistency

--- client/src/components/Caregories.jsx
+++ client/src/components/Caregories.jsx
@@ -55,11 +55,11 @@ const CategoriesPage = () => {
       <img
         src={category.image}
         alt={category.name}
-        className="w-full min-h-auto object-contain md:object-cover position-center"
+        className="w-full object-contain position-center"
       />
       
       <div className="text-center bg-[#B80000] py-2">
-        <h3 className="text-white sm:text-lg md:text-2xl  font-semibold drop-shadow-md">
+        <h3 className="text-white sm:text-lg md:text-2xl capitalize  font-semibold drop-shadow-md">
           {category.name}
         </h3>
       </div>
